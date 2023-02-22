d9bcfb913abc0dd8bd9adc7552045bba9b57747f
Rosen Yordanov <roskata182003@gmail.com>
2023-02-22 17:33:18 +0200

    Upload fixed problem

--- JavaScript/JS Syntax Fundamentals - Exercises/List Of Names/index.js
+++ JavaScript/JS Syntax Fundamentals - Exercises/List Of Names/index.js
@@ -1,8 +1,23 @@
 function solve(array) {
-  array.sort();
+  array.sort((a, b) => a.localeCompare(b));
   let counter = 0;
   for (let index = 0; index < array.length; index++) {
     console.log(`${++counter}.${array[index]}`);
   }
 }
-solve(["John", "Bob", "Christina", "Ema", "G", "Vesko" ,'G',"John", "Bob", "Christina", "Ema", "G", "Vesko" ,'G']);
+solve([
+  "John",
+  "Bob",
+  "Christina",
+  "Ema",
+  "G",
+  "Vesko",
+  "G",
+  "John",
+  "Bob",
+  "Christina",
+  "Ema",
+  "G",
+  "Vesko",
+  "G",
+]);
