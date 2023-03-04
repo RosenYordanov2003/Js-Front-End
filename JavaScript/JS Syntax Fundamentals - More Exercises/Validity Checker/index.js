fcc6a48ca93300ffb24cad4431392e903948eec5
Rosen Stoqnov Yordanov <107473016+RosenYordanov2003@users.noreply.github.com>
2023-03-04 13:20:54 +0200

    Upload new solution

--- JavaScript/JS Syntax Fundamentals - More Exercises/Validity Checker/index.js
+++ JavaScript/JS Syntax Fundamentals - More Exercises/Validity Checker/index.js
@@ -1,27 +1,26 @@
-function solve(x1, x2, y1, y2) {
-  let distance1 = Math.sqrt(x1 ** 2 + y1 ** 2);
-  
-  let distance2 =  Math.sqrt(x2 ** 2 + y2 ** 2);
-
-  let distance3 = Math.sqrt((x2-x1)**2 + (y2-y1)**2);
-
-  if(Number.isInteger(distance1)){
-    console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+function validityChecker(x1, y1, x2, y2) {
+  function checkCoordinates(x1, y1, x2, y2) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
   }
-  else{
+
+  let firstCoordinates = checkCoordinates(x1, y1, 0, 0);
+  if (Number.isInteger(firstCoordinates)) {
+    console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+  } else {
     console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
   }
-  if(Number.isInteger(distance2)){
+
+  let secondCoordinates = checkCoordinates(x2, y2, 0, 0);
+  if (Number.isInteger(secondCoordinates)) {
     console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
-  }
-  else{
+  } else {
     console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
   }
-  if(Number.isInteger(distance3)){
+
+  let thirdCoordinates = checkCoordinates(x1, y1, x2, y2);
+  if (Number.isInteger(thirdCoordinates)) {
     console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
-  }
-  else{
+  } else {
     console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
   }
 }
-solve(2, 1, 1, 1);
